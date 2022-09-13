cc00f494139a71f16aab9914d83949123d654c0b
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-09-14 00:06:48 +0600

    posts added

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Post.css';
 
-const Post = () => {
+const Post = (props) => {
+
+    const {title, body} = props.post;
     return (
-        <div>
-            this is a single post
+        <div className='post' gridColumn="span 4">
+            <h3>{title}</h3>
+            <p>{body}</p>
+            <button> see more</button>
         </div>
     );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Posts.css';
 import Post from './../Post/Post';
 
 const Posts = () => {
+
+    const [posts , setPosts] = useState([]);
+
+    useEffect( () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then( res => res.json())
+            .then( data => setPosts(data))
+    })
     return (
-        <div class='all-posts'>
-            <h1>this is posts</h1>
-            <Post></Post>
+        <div className='all-posts'>
+            {
+                posts.map( po => <Post key={po.id} post={po}></Post>)
+            }
         </div>
     );
 };
